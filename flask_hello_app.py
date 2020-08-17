e7369c8207555fbc789a7bad0bec0be069098f95
chingsley <eneja.kc@gmail.com>
2020-08-17 03:48:18 +0100

    complete introduction to SQLAlchemy

--- flask_hello_app.py
+++ flask_hello_app.py
@@ -4,12 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://kingsleyeneja:chinonxo@localhost:5432/example'
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Person(db.Model):
   __tablename__ = 'persons'
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(), nullable=False)
+
+  def __repr__(self):
+    return f'<Peson {self.id}, {self.name}>'
 
 db.create_all()
 
